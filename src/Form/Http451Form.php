125ef1733cda6f34f04afdb76dc15d8c6fdc9bcc
Kifah Meeran <maskys@cyberstorm.mu>
2018-11-03 16:05:02 +0400

    Refactor, rename 'nid' to 'page_id' and add blocked-by field

--- src/Form/Http451Form.php
+++ src/Form/Http451Form.php
@@ -19,12 +19,13 @@ class Http451Form extends ConfigFormBase {
          *
          * List of fields
          * -----------------------------------
-         * 
+         *
+         * Authority Implementing the censorship
          * Authority Requesting the censorship
          * Custom page title
          * Custom page message
          * -----------------------------------
-         * 
+         *
          * */
         // Invoking Form constructor
         $form = parent::buildForm($form, $form_state);
@@ -33,108 +34,95 @@ class Http451Form extends ConfigFormBase {
         $config = \Drupal::config('http451.settings');
         $default_title = $config->get('page_title');
         $default_content = $config->get('page_content');
-        $form['content_id'] = array (
+        $form['page_id'] = [
             '#type' => 'textfield',
             '#title' => $this->t('Blocked Post ID: '),
             '#description' => $this->t('Enter the ID of the resource you wish to block'),
-            '#required' => TRUE,
-        );
+            '#required' => true
+        ];
 
-        $form['blocking_authority'] = array(
-            '#type' => 'textfield',
+        $form['blocked_by'] = [
+            '#type' => 'url',
+            '#title' => $this->t('URL of Authority Implementing Takedown '),
+            '#description' => $this->t('You need to specify the URL of the entity who implemented the takedown.'),
+            '#required' => true,
+        ];
+
+        $form['blocking_authority'] = [
+            '#type' => 'url',
             '#title' => $this->t('URL of Authority: '),
-            '#required' => TRUE,
             '#description' => $this->t('You need to specify the URL of the authority who requested the takedown.'),
-        );
+            '#required' => true,
+        ];
 
-        $form['page_title'] = array(
+        $form['page_title'] = [
             '#type' => 'textfield',
             '#title' => $this->t('Censored page title: '),
             '#default_value' => $default_title,
             '#description' => $this->t('If you wish to use a custom non-standard title to show up on this page, you can set it here.'),
-        );
+        ];
 
-        $form['page_content'] = array(
+        $form['page_content'] = [
             '#type' => 'textarea',
             '#title' => $this->t('Reason for censorship: '),
             '#default_value' => $default_content,
             '#description' => $this->t('If you wish to use a custom message to show up on this page, you can set it here.'),
-        );
+        ];
 
-        $form['save'] = array (
+        $form['save'] = [
             '#type' => 'submit',
             '#value' => $this->t('Block Item')
-        );
+        ];
 
         return $form;
     }
 
     public function validateForm(array &$form, FormStateInterface $form_state) {
+        // TODO Implement validation.
         return;
     }
 
     public function submitForm(array &$form, FormStateInterface $form_state) {
         $root_dir = realpath(dirname(__FILE__));
         $filename = 'blocked_ids.json';
-        $id = $form_state->getValue('content_id');
-        $authority = $form_state->getValue('blocking_authority');
-        $title = $form_state->getValue('page_title');
-        $content = $form_state->getValue('page_content');
+        $values = $form_state->getValues();
 
         // Make sure the directory is writable
-        // sudo chown -R www-data:www-data Form 
+        // sudo chown -R www-data:www-data Form
         // Append data to blocked_ids.json
-        if(is_writable("$root_dir")) {
-            // Check if file exists
-            if(file_exists("$root_dir/$filename")) {
-                $current_data = file_get_contents("$root_dir/$filename");
-                $data_array = json_decode($current_data, true);
-                $flag = FALSE;
-                foreach($data_array as $node => $attribute) {
-                    if($attribute["nid"] == "$id") {
-                        $flag = TRUE;
-                        $data_array[$node]['authority'] = $authority;
-                        $data_array[$node]['title'] = $title;
-                        $data_array[$node]['content'] = $content;
-                    }
-                }
-
-                if(!$flag) {
-                    $data_array[] = array(
-                        "nid" => $id,
-                        "authority" => $authority,
-                        "title" => $title,
-                        "content" => $content,
-                    );
-                }
-                
-                if(file_put_contents("$root_dir/$filename", json_encode($data_array, JSON_PRETTY_PRINT))) {
-                    drupal_set_message(t('SUCCESS: Message for blocked resource updated!'), 'status');
-                } else {
-                    drupal_set_message(t('ERROR: Could not update the page for this blocked resource'), 'error');
-                }    
-            } else {
-                // create the blocked_ids.json file 
-                $current_data = file_get_contents("$root_dir/$filename");
-                $data_array = json_decode($current_data, true);
-                $data = array(
-                    "nid" => $id,
-                    "authority" => $authority,
-                    "title" => $title,
-                    "content" => $content,
-                );
-                $data_array[] = $data;
-                $data_array = json_encode($data_array, JSON_PRETTY_PRINT);
-                file_put_contents("$root_dir/$filename", $data_array);
-                drupal_set_message(t('SUCCESS: Resource has been successfully blocked!'), 'status');
-                return true;
-            }
-            
-        } else {
-            drupal_set_message(t('Error: Please make sure that the module directory is writable. PATH:' . "$root_dir/$filename"), 'error');
+        // TODO Either find a way to change permissions programatically, or
+        //  specify to run the above command in the README.
+        if(!is_writable("$root_dir")) {
+            drupal_set_message($this->t('Error: Please make sure that the module directory is writable. PATH:' . "$root_dir/$filename"), 'error');
+            return parent::submitForm($form, $form_state);
         }
 
-        return parent::submitForm($form, $form_state);
-        
+        $is_page_already_blocked = false;
+        // Check if file exists
+        if(file_exists("$root_dir/$filename")) {
+            $current_data = file_get_contents("$root_dir/$filename");
+            $data_array = json_decode($current_data, true);
+
+            // Check if this page was already blocked before; if so update details.
+            foreach($data_array as $node => $attribute) {
+                if($attribute['page_id'] == $values['page_id']) {
+                    $is_page_already_blocked = true;
+                    $data_array[$node] = $values;
+                }
+            }
+        }
+
+        if (!$is_page_already_blocked) {
+            $data_array[] = $values;
+        }
+
+        $data_array = json_encode($data_array, JSON_PRETTY_PRINT);
+        $is_file_write_successful = (bool) file_put_contents("$root_dir/$filename", $data_array);
+        if($is_file_write_successful) {
+            drupal_set_message($this->t('SUCCESS: Message for blocked resource updated!'), 'status');
+            return true;
+        } else {
+            drupal_set_message($this->t('ERROR: Could not update the page for this blocked resource'), 'error');
+        }
     }
 }
